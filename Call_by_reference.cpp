d608348028482b093b84ed52e24bc3d083943647
Sahil <118194056+Sahilll94@users.noreply.github.com>
2024-03-16 03:04:23 +0530

    Create Call_by_reference.cpp

---
+++ Call_by_reference.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+
+int main()
+{
+    int a=10;
+    int b=20;
+    cout<<"The numbers are: "<<a<<endl;
+    cout<<"The numbers are: "<<b<<endl;
+    swap(&a,&b);
+    cout<<"The numbers are after swapping: "<<a<<endl;
+    cout<<"The numbers are after swapping: "<<b<<endl;
+}
+
